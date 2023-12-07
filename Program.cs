5e99fc7cd863ad2533f5687163affa2c47f510d5
Battal <Battal@BATTAL>
2023-12-07 17:52:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeKamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double dolarDun = 27.85;
+            //double dolarBugun = 27.85;
+
+            //if (dolarDun<dolarBugun)
+            //{
+            //    Console.WriteLine("Dolar Arttı");
+            //}
+            //else if (dolarDun>dolarBugun)
+            //{
+            //    Console.WriteLine("Dolar Düştü");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Kur Değişmedi");
+
+
+            //}
+
+            //Console.ReadLine();
+            string[] gezilecekYerler = new string[] {"İzmir","İstanbul","Muğla","Kastamonu","Samsun","Kars",
+            "Kırşehir","Çorum","Adana"};
+
+            for (int i = 0; i < gezilecekYerler.Length; i++) 
+            {
+
+                Console.WriteLine(gezilecekYerler[i]);
+
+            }
+            Console.WriteLine("-------  for bitti  ------");
+
+            foreach (string gezilecekYer in gezilecekYerler)
+            {
+                Console.WriteLine(gezilecekYer);
+            }
+
+
+            Console.WriteLine( "Yeni Yerlere Bakalım ^_^ " );
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
